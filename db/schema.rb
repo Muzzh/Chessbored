b96b9578bda5164afcf5051b8db13abbdfb3ef4e
Muzzh <murdyn@gmail.com>
2017-09-11 00:23:10 +0000

    add STI for pieces model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170911000735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.string   "color"
+    t.integer  "x"
+    t.integer  "y"
+    t.boolean  "captured"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
