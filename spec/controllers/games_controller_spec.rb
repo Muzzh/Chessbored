dcbe514bc421ce7e834e8ae573ff5b9fa5b916e1
Ali Thompson <alilynne.thompson@gmail.com>
2017-09-11 23:31:42 +0000

    add controller tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe "games#new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe "games#create action" do
+    it "should successfully create a new game in our database" do
+      post :create
+    end
+  end
 end
