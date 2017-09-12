1f01e0388634202513c9c1685f48fb1479c359ea
Ali Thompson <alilynne.thompson@gmail.com>
2017-09-12 00:17:13 +0000

    added games#new controller test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,10 +7,4 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "games#create action" do
-    it "should successfully create a new game in our database" do
-      post :create
-    end
-  end
 end
