49c93975e4142be50661c53c329f0a2e93f227fc
Muzzh <murdyn@gmail.com>
2017-09-14 00:46:48 +0000

    added scopes to Game model, added queries to pages#index controller#action and a display on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def index
-    
+    @pending_games = Game.by_status('pending').recent
+    @completed_games = Game.by_status('completed').recent
+    @in_progress_games = Game.by_status('in_progress').recent
   end
 end
