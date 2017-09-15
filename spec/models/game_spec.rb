e1b8a930f0ebc1a2f9e03675a9b5514e7c90c6ab
Muzzh <murdyn@gmail.com>
2017-09-15 15:44:28 +0000

    added forgotten tests for other scopes

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe Game, type: :model do
       expect(game.pending?).to eq true
     end
   end
+
+  describe '.completed' do
+    let(:game) { FactoryGirl.create :game, :completed }
+
+    it 'is completed' do
+      expect(game.status).to eq 'completed'
+    end
+
+    it 'is not pending' do
+      expect(game.pending?).to eq false
+    end
+  end
+
+  describe '.in_progress' do
+    let(:game) { FactoryGirl.create :game, :in_progress }
+
+    it 'is completed' do
+      expect(game.status).to eq 'in_progress'
+    end
+
+    it 'is not pending' do
+      expect(game.pending?).to eq false
+    end
+  end
 end
