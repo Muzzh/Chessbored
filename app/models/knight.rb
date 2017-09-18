52b2452e51b016e0a69e494afe62f335a6211757
netdev01 <netdev01@gotomyhome.com>
2017-09-18 20:00:34 +0000

    add valid_move? for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -2,4 +2,28 @@ class Knight < ChessPiece
 
   # Knight specific methods ...
 
+  # the valid offsets form its original location
+	@@offsets = 
+		[	{x:  1, y:  2},
+		 	{x:  1, y: -2},
+			{x: -1, y:  2},
+			{x: -1, y: -2},
+			{x:  2, y:  1},
+			{x:  2, y: -1},
+			{x: -2, y:  1},
+			{x: -2, y: -1}	]
+
+  # iterate through valid offsets to determine valid target locations
+  def valid_move?(x_target, y_target)
+  	return false if same_location?(x_target, y_target)
+  	return false if !in_board?(x_target, y_target)
+  	check = false
+		@@offsets.each do |offset| 
+		  if x_target == x + offset[:x] && y_target == y + offset[:y]
+		  	check = true
+		  end
+		end
+  	return check
+  end
+
 end
--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -3,15 +3,6 @@ require 'rails_helper'
 RSpec.describe ChessPiecesController, type: :controller do
   
   describe 'chess_pieces#create' do
-    it "should create dummy King" do
-      king = FactoryGirl.create(:king)
-      expect(king.game_id).to eq(1)
-      expect(king.user_id).to eq(1)
-      expect(king.x).to eq(1)
-      expect(king.y).to eq(2)
-      expect(king.captured).to eq(false)
-      expect(king.type).to eq('King')
-    end
     it "should allow creating a King" do
       post :create, params: { chess_piece: { user_id: 1, game_id: 1, x: 1, y: 2, captured: false, type: 'King' } }
       expect(King.last.user_id).to eq(1)
@@ -20,28 +11,48 @@ RSpec.describe ChessPiecesController, type: :controller do
       expect(King.last.captured).to eq(false)
       expect(ChessPiece.last.type).to eq('King')
     end
+
     it "should check for a valid move for a King" do
       x= 0; y=0; type="King" # piece at 0,0
       post :create, params: { chess_piece: { user_id: 1, game_id: 1, x: x, y: y, captured: false, type: type } }
-      king = King.last
-      expect(king.valid_move?(0,1)).to eq(true)
-      expect(king.valid_move?(1,0)).to eq(true)
-      expect(king.valid_move?(0,0)).to eq(false)
-      expect(king.valid_move?(1,1)).to eq(false)
-      expect(king.valid_move?(2,2)).to eq(false)
-      expect(king.valid_move?(-1,0)).to eq(false)
+      piece = King.last
+      expect(piece.valid_move?(0,1)).to eq(true)
+      expect(piece.valid_move?(1,0)).to eq(true)
+      expect(piece.valid_move?(0,0)).to eq(false)
+      expect(piece.valid_move?(1,1)).to eq(false)
+      expect(piece.valid_move?(2,2)).to eq(false)
+      expect(piece.valid_move?(-1,0)).to eq(false)
+      piece.x = 0; piece.y = 7
+      expect(piece.valid_move?(0,8)).to eq(false)
     end
 
     it "should check for a valid move for a Rook" do
       x= 2; y=0; type="Rook" # piece at 2,0
       post :create, params: { chess_piece: { user_id: 1, game_id: 1, x: x, y: y, captured: false, type: type } }
-      rook = Rook.last
-      expect(rook.valid_move?(4,2)).to eq(true)
-      expect(rook.valid_move?(1,1)).to eq(true)
-      expect(rook.valid_move?(2,0)).to eq(false)
-      expect(rook.valid_move?(3,2)).to eq(false)
-      expect(rook.valid_move?(-1,0)).to eq(false)
+      piece = Rook.last
+      expect(piece.valid_move?(4,2)).to eq(true)
+      expect(piece.valid_move?(1,1)).to eq(true)
+      expect(piece.valid_move?(2,0)).to eq(false)
+      expect(piece.valid_move?(3,2)).to eq(false)
+      expect(piece.valid_move?(-1,0)).to eq(false)
     end
+
+    it "should check for a valid move for a Knight" do
+      x= 2; y=5; type="Knight" # piece at 2,5
+      post :create, params: { chess_piece: { user_id: 1, game_id: 1, x: x, y: y, captured: false, type: type } }
+      piece = Knight.last
+      expect(piece.valid_move?(3,7)).to eq(true)
+      expect(piece.valid_move?(4,6)).to eq(true)
+      expect(piece.valid_move?(4,4)).to eq(true)
+      expect(piece.valid_move?(3,3)).to eq(true)
+      expect(piece.valid_move?(1,3)).to eq(true)
+      expect(piece.valid_move?(0,4)).to eq(true)
+      expect(piece.valid_move?(0,6)).to eq(true)
+      expect(piece.valid_move?(1,8)).to eq(false)
+      expect(piece.valid_move?(2,6)).to eq(false)
+      expect(piece.valid_move?(3,4)).to eq(false)
+    end
+
   end
 
 end
