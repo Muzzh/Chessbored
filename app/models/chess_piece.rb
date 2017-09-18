95e4625c9bb22e9c9da211ad535e88489aa83c3a
Muzzh <murdyn@gmail.com>
2017-09-18 14:47:51 +0000

    I was told that this stuff in the chess_piece model was depricated and is no longer needed

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,13 +1,5 @@
 class ChessPiece < ApplicationRecord
-  # Will have to add belongs_to :game, :user_id
-  self.inheritance_column = :type
+  # Will have to add belongs_to :game, :user
 
-  def self.types
-    %w(Rook Knight Bishop King Queen Pawn)
-  end
-
-  def self.chess_piece
-    %w(Rook Knight Bishop King Queen Pawn)
-  end
   # Common methods for all pieces ...
 end
