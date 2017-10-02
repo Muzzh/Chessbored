57c97c6fc206f87bff965946dc17fa1b024ec247
netdev01 <netdev01@gotomyhome.com>
2017-10-02 17:05:51 +0000

    fixing spacing/tab issue

--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -1,165 +1,165 @@
 require 'rails_helper'
 
 RSpec.describe ChessPiecesController, type: :controller do
-	
-	describe 'chess_pieces#create' do
+  
+  describe 'chess_pieces#create' do
 
-		it "should create dummy King" do
-			user = FactoryGirl.create(:user)
-			king = FactoryGirl.create(:king, user_id: user.id)
-			expect(king.user_id).to eq(user.id)
-			expect(king.x).to eq(3)
-			expect(king.y).to eq(0)
-			expect(king.captured).to eq(false)
-			expect(king.type).to eq('King')
-		end
+    it "should create dummy King" do
+      user = FactoryGirl.create(:user)
+      king = FactoryGirl.create(:king, user_id: user.id)
+      expect(king.user_id).to eq(user.id)
+      expect(king.x).to eq(3)
+      expect(king.y).to eq(0)
+      expect(king.captured).to eq(false)
+      expect(king.type).to eq('King')
+    end
 
-		it "should check for a valid move for a King" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:king, user_id: user.id)
-			piece.x = 4; piece.y = 4; piece.color = "white"
-			# valid move
-			expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true) 
-			expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
-			# invalid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(false)
-			expect(piece.valid_move?(piece.x+0, piece.y-3)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y-3)).to eq(false)
-		end
+    it "should check for a valid move for a King" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:king, user_id: user.id)
+      piece.x = 4; piece.y = 4; piece.color = "white"
+      # valid move
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true) 
+      expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
+      # invalid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(false)
+      expect(piece.valid_move?(piece.x+0, piece.y-3)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y-3)).to eq(false)
+    end
 
-		it "should check for a valid move for a Bishop" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:bishop, user_id: user.id)
-			piece.x = 3; piece.y = 3; piece.color = "white";
-			# valid move
-			expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)		# 3 step move
-			expect(piece.valid_move?(piece.x+3, piece.y-3)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y+3)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y-3)).to eq(true)
-			expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y-1)).to eq(true)		# 1 step move
-			# invalid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+3, piece.y-2)).to eq(false)
-			expect(piece.valid_move?(piece.x+3, piece.y+2)).to eq(false)
-			expect(piece.valid_move?(piece.x+8, piece.y+8)).to eq(false)
-		end
+    it "should check for a valid move for a Bishop" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:bishop, user_id: user.id)
+      piece.x = 3; piece.y = 3; piece.color = "white";
+      # valid move
+      expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)   # 3 step move
+      expect(piece.valid_move?(piece.x+3, piece.y-3)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y+3)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y-3)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y-1)).to eq(true)   # 1 step move
+      # invalid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+3, piece.y-2)).to eq(false)
+      expect(piece.valid_move?(piece.x+3, piece.y+2)).to eq(false)
+      expect(piece.valid_move?(piece.x+8, piece.y+8)).to eq(false)
+    end
 
-		it "should check for a valid move for a Rook" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:rook, user_id: user.id)
-			piece.x = 3; piece.y = 3; piece.color = "white";
-			# valid move
-			expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)
-			expect(piece.valid_move?(piece.x+3, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
-			# invalid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y+3)).to eq(false)
-			expect(piece.valid_move?(piece.x-3, piece.y+4)).to eq(false)
-			expect(piece.valid_move?(piece.x+0, piece.y+8)).to eq(false)
-			expect(piece.valid_move?(piece.x-5, piece.y+0)).to eq(false)
-		end
+    it "should check for a valid move for a Rook" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:rook, user_id: user.id)
+      piece.x = 3; piece.y = 3; piece.color = "white";
+      # valid move
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
+      # invalid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+3)).to eq(false)
+      expect(piece.valid_move?(piece.x-3, piece.y+4)).to eq(false)
+      expect(piece.valid_move?(piece.x+0, piece.y+8)).to eq(false)
+      expect(piece.valid_move?(piece.x-5, piece.y+0)).to eq(false)
+    end
 
-	 it "should check for a valid move for a Queen" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:queen, user_id: user.id)
-			piece.x = 3; piece.y = 3; piece.color = "white";
-			# valid  moves for rook
-			expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)
-			expect(piece.valid_move?(piece.x+3, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
-			# valid moves for bishop
-			expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
-			expect(piece.valid_move?(piece.x+3, piece.y-3)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y+3)).to eq(true)
-			expect(piece.valid_move?(piece.x-3, piece.y-3)).to eq(true)
-			expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y-1)).to eq(true)
-			# invalid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y+3)).to eq(false)
-			expect(piece.valid_move?(piece.x+3, piece.y-2)).to eq(false)
-			expect(piece.valid_move?(piece.x-3, piece.y+2)).to eq(false)
-			expect(piece.valid_move?(piece.x-3, piece.y-2)).to eq(false)
-			expect(piece.valid_move?(piece.x+0, piece.y+8)).to eq(false)
-		end
+   it "should check for a valid move for a Queen" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:queen, user_id: user.id)
+      piece.x = 3; piece.y = 3; piece.color = "white";
+      # valid  moves for rook
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
+      # valid moves for bishop
+      expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y-3)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y+3)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y-3)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y+3)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y-1)).to eq(true)
+      # invalid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+3)).to eq(false)
+      expect(piece.valid_move?(piece.x+3, piece.y-2)).to eq(false)
+      expect(piece.valid_move?(piece.x-3, piece.y+2)).to eq(false)
+      expect(piece.valid_move?(piece.x-3, piece.y-2)).to eq(false)
+      expect(piece.valid_move?(piece.x+0, piece.y+8)).to eq(false)
+    end
 
-		it "should check for a valid move for a Knight" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:knight, user_id: user.id)
-			piece.x = 2; piece.y = 5; piece.color = "white";
-			# valid move
-			expect(piece.valid_move?(piece.x+2, piece.y+1)).to eq(true) 
-			expect(piece.valid_move?(piece.x+2, piece.y-1)).to eq(true)
-			expect(piece.valid_move?(piece.x-2, piece.y+1)).to eq(true) 
-			expect(piece.valid_move?(piece.x-2, piece.y-1)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y+2)).to eq(true)
-			expect(piece.valid_move?(piece.x+1, piece.y-2)).to eq(true)
-			expect(piece.valid_move?(piece.x-1, piece.y+2)).to eq(true)
-			expect(piece.valid_move?(piece.x-1, piece.y-2)).to eq(true)
-			# invalid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x-2, piece.y-2)).to eq(false)
-			expect(piece.valid_move?(piece.x+1, piece.y+3)).to eq(false)
-			expect(piece.valid_move?(piece.x+8, piece.y+0)).to eq(false)
-		end
+    it "should check for a valid move for a Knight" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:knight, user_id: user.id)
+      piece.x = 2; piece.y = 5; piece.color = "white";
+      # valid move
+      expect(piece.valid_move?(piece.x+2, piece.y+1)).to eq(true) 
+      expect(piece.valid_move?(piece.x+2, piece.y-1)).to eq(true)
+      expect(piece.valid_move?(piece.x-2, piece.y+1)).to eq(true) 
+      expect(piece.valid_move?(piece.x-2, piece.y-1)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y-2)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y-2)).to eq(true)
+      # invalid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x-2, piece.y-2)).to eq(false)
+      expect(piece.valid_move?(piece.x+1, piece.y+3)).to eq(false)
+      expect(piece.valid_move?(piece.x+8, piece.y+0)).to eq(false)
+    end
 
-		it "should check for a valid move for a Pawn" do
-			user = FactoryGirl.create(:user)
-			piece = FactoryGirl.create(:pawn, user_id: user.id)
+    it "should check for a valid move for a Pawn" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:pawn, user_id: user.id)
 
-			# white pawn at 2,1
-			piece.x = 2; piece.y = 1; piece.color = "white";
-			# valid move
-			expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true) 
-			expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)		# up 2 steps
-			expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
-			# invalid move
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(false)	# invalid move - back 1 step
-			expect(piece.valid_move?(piece.x+0, piece.y+3)).to eq(false)	# invalid move - up 3 steps
-			expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x-2, piece.y+0)).to eq(false)
-			expect(piece.valid_move?(piece.x+2, piece.y+2)).to eq(false)
+      # white pawn at 2,1
+      piece.x = 2; piece.y = 1; piece.color = "white";
+      # valid move
+      expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true) 
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)   # up 2 steps
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
+      # invalid move
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(false)  # invalid move - back 1 step
+      expect(piece.valid_move?(piece.x+0, piece.y+3)).to eq(false)  # invalid move - up 3 steps
+      expect(piece.valid_move?(piece.x+2, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x-2, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+2)).to eq(false)
 
-			# white pawn at 1,7
-			piece.x = 1; piece.y = 7; piece.color = "white";
-			# valid moves
-			expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true)   # pass the board
-			# invalid move
-			expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(false)  
+      # white pawn at 1,7
+      piece.x = 1; piece.y = 7; piece.color = "white";
+      # valid moves
+      expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(true)   # pass the board
+      # invalid move
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(false)  
 
-			# black pawn at 1,6
-			piece.x = 1; piece.y = 6; piece.color = "black";
-			# valid moves
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true) 	# down 1 step
-			expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)		# down 2 steps
-			expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
-			expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
-			# invalid move
-			expect(piece.valid_move?(piece.x+0, piece.y+11)).to eq(false)	# invalid move - back 1 step
+      # black pawn at 1,6
+      piece.x = 1; piece.y = 6; piece.color = "black";
+      # valid moves
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)   # down 1 step
+      expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)   # down 2 steps
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y+0)).to eq(true)
+      # invalid move
+      expect(piece.valid_move?(piece.x+0, piece.y+11)).to eq(false) # invalid move - back 1 step
 
-			# black pawn at 1,0
-			piece.x = 1; piece.y = 0; piece.color = "black";
-			# valid moves
-			expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)		# pass the board
-			# invalid move
-			expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(false)	# invalid move
-		end
+      # black pawn at 1,0
+      piece.x = 1; piece.y = 0; piece.color = "black";
+      # valid moves
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)   # pass the board
+      # invalid move
+      expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(false)  # invalid move
+    end
 
-	end
+  end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,89 +1,89 @@
 FactoryGirl.define do
-	factory :user do
-		sequence :email do |n|
-			"test#{n}@test.user"
-		end
-		password 'mypassword'
-		password_confirmation 'mypassword'
-	end
+  factory :user do
+    sequence :email do |n|
+      "test#{n}@test.user"
+    end
+    password 'mypassword'
+    password_confirmation 'mypassword'
+  end
 
-	factory :rook do
-		game
-		type      'Rook'
-		user_id   1
-		x         0
-		y         0
-		captured  false
-		color     'white'
-	end
+  factory :rook do
+    game
+    type      'Rook'
+    user_id   1
+    x         0
+    y         0
+    captured  false
+    color     'white'
+  end
 
-	factory :knight do
-		game
-		type      'Knight'
-		user_id   1
-		x         1
-		y         0
-		captured  false
-		color     'white'
-	end
+  factory :knight do
+    game
+    type      'Knight'
+    user_id   1
+    x         1
+    y         0
+    captured  false
+    color     'white'
+  end
 
-	factory :bishop do
-		game
-		type      'Bishop'
-		user_id   1
-		x         2
-		y         0
-		captured  false
-		color     'white'
-	end
+  factory :bishop do
+    game
+    type      'Bishop'
+    user_id   1
+    x         2
+    y         0
+    captured  false
+    color     'white'
+  end
 
-	factory :king do
-		game
-		type      'King'
-		user_id   1
-		x         3
-		y         0
-		captured  false
-		color     'white'
-	end
+  factory :king do
+    game
+    type      'King'
+    user_id   1
+    x         3
+    y         0
+    captured  false
+    color     'white'
+  end
 
-	factory :queen do
-		game
-		type      'Queen'
-		user_id   1
-		x         4
-		y         0
-		captured  false
-		color     'white'
-	end
+  factory :queen do
+    game
+    type      'Queen'
+    user_id   1
+    x         4
+    y         0
+    captured  false
+    color     'white'
+  end
 
-	factory :pawn do
-		game
-		type      'Pawn'
-		user_id   1
-		x         0
-		y         1
-		captured  false
-		color     'white'
-	end
+  factory :pawn do
+    game
+    type      'Pawn'
+    user_id   1
+    x         0
+    y         1
+    captured  false
+    color     'white'
+  end
 
-	factory :game do
-		trait :pending do
-			status 'pending'
-			black_player_id nil
-		end
+  factory :game do
+    trait :pending do
+      status 'pending'
+      black_player_id nil
+    end
 
-		trait :completed do
-			status 'completed'
-		end
+    trait :completed do
+      status 'completed'
+    end
 
-		trait :in_progress do
-			status 'in_progress'
-		end
-		black_player_id 1
-		white_player_id 2
-		winner_id       nil
+    trait :in_progress do
+      status 'in_progress'
+    end
+    black_player_id 1
+    white_player_id 2
+    winner_id       nil
 
-	end
+  end
 
 end
