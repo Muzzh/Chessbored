958b64f7d810ea216900b0be4975250f688495a7
Muzzh <murdyn@gmail.com>
2017-10-02 22:16:12 +0000

    initial methods

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -3,8 +3,33 @@ class ChessPiece < ApplicationRecord
   belongs_to :user
 
 
-  def color
-    user.id == game.white_player_id ? 'white' : 'black'
+  private
+
+  def is_occupied?(x_current, y_current)
+    game.pieces.where(x: x_current, y: y_current).present?
+  end
+
+  def is_obstructed?(x_target, y_target)
+    if move_diagonally?
+
+    end
+  end
+
+  def horizontal?
+    
+  end
+
+  def move_diagonally?(x_target, y_target)
+    x_dist = (x_target - x).abs
+    y_dist = (y_target - y).abs
+    return true if x_dist == y_dist
+    return false
+  end
+
+  def vertical?
+    
   end
   # Common methods for all pieces ...
 end
+
+
