d99ef849681de7a7b5ea2530fb1f4db988fab413
Muzzh <murdyn@gmail.com>
2017-10-03 23:36:19 +0000

    test for chess_piece#update

--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe ChessPiecesController, type: :controller do
   
   describe 'chess_pieces#create' do
-    it "should create dummy King" do
 
+    it "should create dummy King" do
       user = FactoryGirl.create(:user)
       king = FactoryGirl.create(:king, user_id: user.id)
 
@@ -13,6 +13,19 @@ RSpec.describe ChessPiecesController, type: :controller do
       expect(king.y).to eq(0)
       expect(king.captured).to eq(false)
       expect(king.type).to eq('King')
+    end
+  end
+
+  describe 'chess_pieces#update' do
+
+    it 'should change coordinates of the moved piece' do
+      user1 = FactoryGirl.create(:user)
+      game = FactoryGirl.create(:game, white_player_id: user1.id)
+      piece = FactoryGirl.create(:king, user_id: user1.id, game_id: game.id)
+      put :update, params: { id: piece.id, x_target: 4, y_target: 4 }
+      piece.reload
+      expect(piece.x).to eq(4)
+      expect(piece.y).to eq(4)
     end
   end
 
