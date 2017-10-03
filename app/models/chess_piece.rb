95d8beaa6261b2c54c689acda08f0849eedf912f
Muzzh <murdyn@gmail.com>
2017-10-03 21:46:06 +0000

    horizontal and vertical checks

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -3,31 +3,59 @@ class ChessPiece < ApplicationRecord
   belongs_to :user
 
 
-  private
-
-  def is_occupied?(x_current, y_current)
-    game.pieces.where(x: x_current, y: y_current).present?
-  end
 
   def is_obstructed?(x_target, y_target)
-    if move_diagonally?
-
+    # determine direction
+    
+    if move_direction(x_target, y_target) == 'horizontal'
+      if x_target > x
+        (x + 1).upto(x_target -1) do |x_current|
+          return true if occupied?(x_current, y)
+        end
+      else
+        (x - 1).downto(x_target + 1) do |x_current|
+          return true if occupied?(x_current, y)
+        end
+      end
+    elsif move_direction(x_target, y_target) == 'vertical'
+      if y_target > y
+        (y + 1).upto(y_target -1) do |y_current|
+          return true if occupied?(x, y_current)
+        end
+      else
+        (y - 1).downto(y_target + 1) do |y_current|
+          return true if occupied?(x, y_current)
+        end
+      end
+    elsif move_direction(x_target, y_target) == 'diagonal'
+      # up and right
+      if x_target > x && y_target > y
+      
+      # up and left
+      elsif x_target < x && y_target > y
+        
+      # down and right
+      elsif x_target > x && y_target < y
+        
+      # down and left
+      else
+      
+      end
     end
-  end
-
-  def horizontal?
-    
-  end
-
-  def move_diagonally?(x_target, y_target)
-    x_dist = (x_target - x).abs
-    y_dist = (y_target - y).abs
-    return true if x_dist == y_dist
     return false
+    # determine each spot between start and target
+    # check presence of a piece at each spot
   end
 
-  def vertical?
-    
+  def move_direction(x_target, y_target)
+    return 'horizontal' if x != x_target && y == y_target
+    return 'vertical' if x == x_target && y != y_target
+    return 'diagonal' if (x_target - x).abs == (y_target - y).abs
+    false
+  end
+
+  def occupied?(x_current, y_current)
+    game.chess_pieces.where(x: x_current, y: y_current).present?
   end
   # Common methods for all pieces ...
 end
--- spec/models/chess_piece_spec.rb
+++ spec/models/chess_piece_spec.rb
@@ -2,4 +2,96 @@ require 'rails_helper'
 
 RSpec.describe ChessPiece, type: :model do
   it_behaves_like "an STI class"
+
+  describe '.is_obstructed?' do
+    let(:user1) { FactoryGirl.create(:user) }
+    let(:user2) { FactoryGirl.create(:user) }
+    let(:game) { FactoryGirl.create(:game) }
+
+    # Horizontal
+    it 'should return true - Horizontal Right YES obstruction' do
+      rook = FactoryGirl.create(:rook, x: 4, y: 4, user_id: user1.id, game_id: game.id)
+      knight = FactoryGirl.create(:bishop, x: 6, y: 4, user_id: user1.id, game_id: game.id)
+      # start 4, 4 => 7, 4 -- piece at 6, 4 
+      expect(rook.is_obstructed?(7, 4)).to eq(true)
+    end
+
+    it 'should return false - Horizontal Right NO obstruction' do
+      rook = FactoryGirl.create(:rook, x: 4, y: 4, user_id: user1.id, game_id: game.id)
+      # start 4, 4 => 7, 4
+      expect(rook.is_obstructed?(7, 4)).to eq(false)
+    end
+
+    it 'should return true - Horizontal Left YES obstruction' do
+      rook = FactoryGirl.create(:rook, x: 4, y: 4, user_id: user1.id, game_id: game.id)
+      knight = FactoryGirl.create(:bishop, x: 3, y: 4, user_id: user1.id, game_id: game.id)
+      # start 4, 4 => 2, 4 -- piece at 3, 4
+      expect(rook.is_obstructed?(2, 4)).to eq(true)
+    end
+
+    it 'should return false - Horizontal Left NO obstruction' do
+      rook = FactoryGirl.create(:rook, x: 4, y: 4, user_id: user1.id, game_id: game.id)
+      # start 4, 4 => 2, 4
+      expect(rook.is_obstructed?(2, 4)).to eq(false)
+    end
+
+    # Vertical
+    it 'should return true - Horizontal Up YES obstruction' do
+      rook = FactoryGirl.create(:rook, x: 2, y: 2, user_id: user1.id, game_id: game.id)
+      knight = FactoryGirl.create(:bishop, x: 2, y: 4, user_id: user1.id, game_id: game.id)
+      # start 2, 2 => 2, 5 -- piece at 2, 4
+      expect(rook.is_obstructed?(2, 5)).to eq(true)
+    end
+
+    it 'should return false - Horizontal Up NO obstruction' do
+      rook = FactoryGirl.create(:rook, x: 2, y: 2, user_id: user1.id, game_id: game.id)
+      # start 2, 2 => 2, 5
+      expect(rook.is_obstructed?(2, 5)).to eq(false)
+    end
+
+    it 'should return true - Horizontal Down YES obstruction' do
+      rook = FactoryGirl.create(:rook, x: 2, y: 5, user_id: user1.id, game_id: game.id)
+      knight = FactoryGirl.create(:bishop, x: 2, y: 4, user_id: user1.id, game_id: game.id)
+      # start 2, 5 => 2, 2 piece at 2, 4
+      expect(rook.is_obstructed?(2, 2)).to eq(true)
+    end
+
+    it 'should return false - Horizontal Down NO obstruction' do
+      rook = FactoryGirl.create(:rook, x: 2, y: 5, user_id: user1.id, game_id: game.id)
+      # start 2, 5 => 2, 2
+      expect(rook.is_obstructed?(2, 2)).to eq(false)
+    end
+
+    it 'should return true - Diagonal Up Right YES obstruction' do
+
+    end
+
+    it 'should return false - Diagonal Up Right NO obstruction' do
+
+    end
+
+    it 'should return true - Diagonal Up Left YES obstruction' do
+
+    end
+
+    it 'should return false - Diagonal Up Left NO obstruction' do
+
+    end
+
+    it 'should return true - Diagonal Down Right YES obstruction' do
+
+    end
+
+    it 'should return false - Diagonal Down Right NO obstruction' do
+
+    end
+
+    it 'should return true - Diagonal Down Left YES obstruction' do
+
+    end
+
+    it 'should return false - Diagonal Down Left NO obstruction' do
+
+    end
+  end
 end
