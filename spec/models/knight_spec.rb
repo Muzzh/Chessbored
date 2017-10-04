eb4e697776ba8d0b64420df5ece529888391e329
netdev01 <netdev01@gotomyhome.com>
2017-10-04 03:54:53 +0000

    move rspec test from controller to model

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Knight, type: :class do
-  it_behaves_like "an STI class"
+
+  describe '.valid_move?' do
+    it "should check for valid move for a Knight" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:knight, user_id: user.id)
+      piece.x = 2; piece.y = 5; piece.color = "white";
+      # valid move
+      expect(piece.valid_move?(piece.x+2, piece.y+1)).to eq(true) 
+      expect(piece.valid_move?(piece.x+2, piece.y-1)).to eq(true)
+      expect(piece.valid_move?(piece.x-2, piece.y+1)).to eq(true) 
+      expect(piece.valid_move?(piece.x-2, piece.y-1)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y-2)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x-1, piece.y-2)).to eq(true)
+    end
+    it "should check for invalid move for a Knight" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:knight, user_id: user.id)
+      piece.x = 2; piece.y = 5; piece.color = "white";
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x-2, piece.y-2)).to eq(false)
+      expect(piece.valid_move?(piece.x+1, piece.y+3)).to eq(false)
+      expect(piece.valid_move?(piece.x+8, piece.y+0)).to eq(false)
+    end
+  end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :class do
-  it_behaves_like "an STI class"
+  describe '.valid_move?' do
+    it "should check for valid move for a Rook" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:rook, user_id: user.id)
+      piece.x = 3; piece.y = 3; piece.color = "white";
+      expect(piece.valid_move?(piece.x+0, piece.y+2)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-2)).to eq(true)
+      expect(piece.valid_move?(piece.x+3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x-3, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+1, piece.y+0)).to eq(true)
+      expect(piece.valid_move?(piece.x+0, piece.y-1)).to eq(true)
+    end
+    it "should check for invalid move for a Rook" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:rook, user_id: user.id)
+      piece.x = 3; piece.y = 3; piece.color = "white";
+      expect(piece.valid_move?(piece.x+0, piece.y+0)).to eq(false)
+      expect(piece.valid_move?(piece.x+2, piece.y+3)).to eq(false)
+      expect(piece.valid_move?(piece.x-3, piece.y+4)).to eq(false)
+      expect(piece.valid_move?(piece.x+0, piece.y+8)).to eq(false)
+      expect(piece.valid_move?(piece.x-5, piece.y+0)).to eq(false)
+    end
+  end
 end
