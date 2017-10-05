8317970b632691d37ce531211c3348fe8d050e79
Muzzh <murdyn@gmail.com>
2017-10-05 23:59:25 +0000

    broke down into smaller methods

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -2,72 +2,18 @@ class ChessPiece < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  def color
-    user.id == game.white_player_id ? 'white' : 'black'
-  end
-
   # this will be called inside valid_move? method
   def obstructed?(x_target, y_target)
-    # determine direction
-  
-    if horizontal_move?(x_target, y_target)
-      return true if horizontal_obstruction?(x_target, y_target)
-    elsif vertical_move?(x_target, y_target)
-      if y_target > y
-        # up
-        (y + 1).upto(y_target - 1) do |y_current|
-          return true if occupied?(x, y_current)
-        end
+    case
+      when horizontal_move?(x_target, y_target)
+        horizontal_obstruction?(x_target)
+      when vertical_move?(x_target, y_target)
+        vertical_obstruction?(y_target)
+      when diagonal_move?(x_target, y_target)
+        diagonal_obstruction?(x_target, y_target)
       else
-        # down
-        (y - 1).downto(y_target + 1) do |y_current|
-          return true if occupied?(x, y_current)
-        end
-      end
-    elsif diagonal_move?(x_target, y_target)
-      # up and right
-      if x_target > x && y_target > y
-        (x + 1).upto(x_target - 1) do |x_current|
-          y_current = y + (x_current - x)
-          return true if occupied?(x_current, y_current)
-        end
-
-      # up and left
-      elsif x_target < x && y_target > y
-        (x - 1).downto(x_target + 1) do |x_current|
-          y_current = y + (x_current - x).abs
-          return true if occupied?(x_current, y_current)
-        end
-      # down and right
-      elsif x_target > x && y_target < y
-        (x + 1).upto(x_target - 1) do |x_current|
-          y_current = y - (x_current - x)
-          return true if occupied?(x_current, y_current)
-        end
-      # down and left
-      else
-        (x - 1).downto(x_target + 1) do |x_current|
-          y_current = y - (x_current - x).abs
-          return true if occupied?(x_current, y_current)
-        end
-      end
+        false
     end
-    false
-  end
-
-  def horizontal_obstruction?(x_target, y_target)
-    if x_target > x
-      # right
-      (x + 1).upto(x_target - 1) do |x_current|
-        return true if occupied?(x_current, y)
-      end
-    else
-      # left
-      (x - 1).downto(x_target + 1) do |x_current|
-        return true if occupied?(x_current, y)
-      end
-    end
-    false
   end
 
   def horizontal_move?(x_target, y_target)
@@ -82,11 +28,35 @@ class ChessPiece < ApplicationRecord
     (x_target - x).abs == (y_target - y).abs
   end
 
+  def horizontal_obstruction?(x_target)
+    direction = x_target > x ? 1 : -1
+    (x + direction).step(x_target - direction, direction) do |x_current|
+      return true if occupied?(x_current, y)
+    end
+    false
+  end
+
+  def vertical_obstruction?(y_target)
+    direction = y_target > y ? 1 : -1
+    (y + direction).step(y_target - direction, direction) do |y_current|
+      return true if occupied?(x, y_current)
+    end
+    false
+  end
+
+  def diagonal_obstruction?(x_target, y_target)
+    x_direction = x_target > x ? 1 : -1
+    y_direction = y_target > y ? 1 : -1
+    (x + x_direction).step(x_target - x_direction, x_direction) do |x_current|
+      y_current = y + ((x_current - x).abs * y_direction)
+      return true if occupied?(x_current, y_current)
+    end
+    false
+  end
+
   def occupied?(x_current, y_current)
     game.chess_pieces.where(x: x_current, y: y_current).present?
   end
-
-  # Common methods for all pieces ...
 end
 
 
