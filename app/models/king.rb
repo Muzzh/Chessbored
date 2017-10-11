2c5c25a1b49cea58951f36843d9eb29d5dfe7a29
Ali Thompson <alilynne.thompson@gmail.com>
2017-10-11 17:58:51 +0000

    added valid_castle_move

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,29 @@
 class King < ChessPiece
-  
+
   # King specific methods ...
   def valid_move?(x_target, y_target)
     return false if same_location?(x_target, y_target)
     return false if !in_board?(x_target, y_target)
     return true if move_single_step?(x_target, y_target)
     return false
+  end
+
+  def valid_castle_move?
+    if @king.moved?
+      return false
+    end
+    #has king moved?
+    #has rook moved?
+    #is there an obstruction?
+  end
+
+  def moved?
+    if created_at != updated_at
+      return true
+    end
+  end
+
+  def rook_moved?
   end
 
 end
