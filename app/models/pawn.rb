30a0455fc831d0edcb04011f57a624e8102f50bd
Teresa Barin <tbarin@gmail.com>
2017-10-18 23:46:57 +0000

    added method for opponent moved two spaces

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,6 +6,20 @@ class Pawn < ChessPiece
     return true if color == 'black' && y == 6 && x == x_target && y_target == 4 # 1st move
     return true if color == 'white' && move_single_step?(x_target, y_target) && y_target > y
     return true if color == 'black' && move_single_step?(x_target, y_target) && y_target < y
+    #return true for en passant for white to capture black
+    #return true if color == 'white' && 
+    #return true for en passant for black to capture white
+    #return true if color == 'black' &&
+    false
+  end
+
+  def white_pawn_just_moved_two?(x_target, y_target)
+    return true if valid_move?(x_target, y_target) && color == 'white' && y == 1 && x == x_target && y_target == 3
+    false
+  end
+
+  def black_pawn_just_moved_two?(x_target, y_target)
+    return true if valid_move?(x_target, y_target) && color == 'black' && y == 6 && x == x_target && y_target == 4
     false
   end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -26,4 +26,48 @@ RSpec.describe Pawn, type: :class do
       expect(piece.valid_move?(piece.x+0, piece.y+1)).to eq(false) # invalid move - back 1 step
     end
   end
+
+  describe '.white_pawn_just_moved_two?' do
+    it "should check for if white Pawn just moved two spaces for its first move" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:pawn, user_id: user.id)
+      piece.x = 2; piece.y = 1; piece.color = "white";
+      expect(piece.white_pawn_just_moved_two?(piece.x+0, piece.y+2)).to eq(true)
+      expect(piece.white_pawn_just_moved_two?(piece.x+0, piece.y+1)).to eq(false)
+      expect(piece.white_pawn_just_moved_two?(piece.x+0, piece.y+3)).to eq(false)
+      #white pawn just made first move of two steps
+    end
+  end
+
+  describe '.black_pawn_just_moved_two?' do
+    it "should check for if black Pawn just moved two spaces for its first move" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:pawn, user_id: user.id)
+      piece.x = 1; piece.y = 6; piece.color = "black";
+      expect(piece.black_pawn_just_moved_two?(piece.x+0, piece.y-2)).to eq(true)
+      expect(piece.black_pawn_just_moved_two?(piece.x+0, piece.y-1)).to eq(false)
+      expect(piece.black_pawn_just_moved_two?(piece.x+0, piece.y-0)).to eq(false)
+      #black pawn just made first move of two steps
+    end
+  end
+
+  describe '.en_passant' do
+    it "should check for valid en passant move for a white Pawn" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:pawn, user_id: user.id)
+      piece.x = 2; piece.y = 1; piece.color = "white";
+      #black pawn just made first move of two steps
+      #white pawn could have made capture if pawn had moved only one step
+      #white pawn makes capture
+    end
+
+    it "should check for valid en passant move for a black Pawn" do
+      user = FactoryGirl.create(:user)
+      piece = FactoryGirl.create(:pawn, user_id: user.id)
+      piece.x = 1; piece.y = 6; piece.color = "black";
+      #white pawn just made first move of two steps
+      #black pawn could have made capture if pawn had moved only one step
+      #black pawn makes capture
+    end
+  end
 end
