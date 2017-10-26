8ab4fea3c8a6a19e537d8d3d399eba349b4da2d6
netdev01 <netdev01@gotomyhome.com>
2017-10-26 08:50:37 +0000

    fix rspec

--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -18,14 +18,16 @@ RSpec.describe ChessPiecesController, type: :controller do
     let(:user1) { FactoryGirl.create(:user) }
     let(:user2) { FactoryGirl.create(:user) }
 
-    # it 'should change pawn location on VALID move' do
-    #   game = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
-    #   piece = FactoryGirl.create(:pawn, user_id: user1.id, game_id: game.id, x: 0, y: 1)
-    #   put :update, params: { id: piece.id, x_target: 0, y_target: 2 } # pawn 0, 1 => 0, 2
-    #   piece.reload
-    #   expect(piece.x).to eq(0)
-    #   expect(piece.y).to eq(2)
-    # end
+    it 'should change pawn location on VALID move' do
+      game  = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
+      piece = FactoryGirl.create(:pawn, user_id: user1.id, game_id: game.id, x: 0, y: 1)
+      king  = FactoryGirl.create(:king, color: 'white', x: 3, y: 1, user_id: user1.id, game_id: game.id, )
+      king2 = FactoryGirl.create(:king, color: 'black', x: 3, y: 7, user_id: user1.id, game_id: game.id, )
+      put :update, params: { id: piece.id, x_target: 0, y_target: 2 } # pawn 0, 1 => 0, 2
+      piece.reload
+      expect(piece.x).to eq(0)
+      expect(piece.y).to eq(2)
+    end
 
     it 'should NOT change pawn location on INVALID move' do
       game = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
@@ -36,16 +38,18 @@ RSpec.describe ChessPiecesController, type: :controller do
       expect(piece.y).to eq(1)
     end
 
-    # it 'should indicate check condition with the move' do
-    #   game = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
-    #   king2   = FactoryGirl.create(
-    #       :king,  user_id: user2.id, color: 'black', game_id: game.id, x: 6,y: 6) # king2  6, 6
-    #   queen1  = FactoryGirl.create(
-    #       :queen, user_id: user1.id, color: 'white', game_id: game.id, x: 1,y: 1) # queen1 1, 1
-    #   put :update, params: { id: queen1.id, x_target: 6, y_target: 1 }            # queen1 1,1 => 6,1
-    #   game.reload
-    #   expect(game.status).to eq("in_check")
-    # end
+    it 'should indicate check condition with the move' do
+      game = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
+      king2   = FactoryGirl.create(
+          :king,  user_id: user2.id, color: 'black', game_id: game.id, x: 6,y: 6) # king2  6, 6
+      king1   = FactoryGirl.create(
+          :king,  user_id: user1.id, color: 'white', game_id: game.id, x: 7,y: 1)
+      queen1  = FactoryGirl.create(
+          :queen, user_id: user1.id, color: 'white', game_id: game.id, x: 1,y: 1) # queen1 1, 1
+      put :update, params: { id: queen1.id, x_target: 6, y_target: 1 }            # queen1 1,1 => 6,1
+      game.reload
+      expect(game.status).to eq("in_check")
+    end
 
     it 'should prevent illegal move' do
       game = FactoryGirl.create(:game, white_player_id: user1, black_player_id: user2)
