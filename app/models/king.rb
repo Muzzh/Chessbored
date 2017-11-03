8b31ca280a7261ad0b112693a32e5a31b0d1cd25
Muzzh <murdyn@gmail.com>
2017-11-03 17:04:01 +0000

    initial commit with comments for castling

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,4 +4,16 @@ class King < ChessPiece
     return false unless super
     move_single_step?(x_target, y_target)
   end
+
+  def castling(x_target, y_target)
+    # king has not moved
+
+    # rook has not moved
+
+    # not in check
+
+    # no space that king moves through is in check
+
+    # illegal_move
+  end
 end
