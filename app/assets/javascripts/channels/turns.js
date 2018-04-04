b820eb3e395ba9f61fdbc2c256018facff47b375
Muzzh <murdyn@gmail.com>
2018-04-04 08:00:24 +0000

    another refresh fix attempt

--- app/assets/javascripts/channels/turns.js
+++ app/assets/javascripts/channels/turns.js
@@ -3,14 +3,16 @@
 App.messages = App.cable.subscriptions.create('TurnsChannel', {  
   received: function(data) {
     var gameID = $('.board').data('game-id');
+    console.log(data)
     var windowUserId = $('.board').data('user-id');
     if(data.refresh == true && data.game_id == gameID) {
       if(data.user_played_id == windowUserId) {
+        window.location.replace(data.game_path);
         if (data.error_pop_up != null) alert(data.error_pop_up);
       } else {
-        if (data.turn_pop_up != null) alert(data.turn_pop_up)
+        window.location.replace(data.game_path);
+        if (data.turn_pop_up != null) alert(data.turn_pop_up);
       }
     }
-    location.replace(data.game_path);
   }
 });
